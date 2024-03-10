0986cfdf82c536fca769ea77ed1b109abd684092
Cviex92 <cviex92@gmail.com>
2024-03-10 14:39:35 +0000

    Add the search functionality using the API
    I have an issue, the API key is not valid, I have asked to be provided a new one by the team, I will update the code and check if it works.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-function hadleSearchSubmit(event){
+function refreshWeather(response){
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  
+  cityElement.innerHTML= response.data.city; 
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+
+function searchCity (city){
+ //make api call and update the interface,receive the city and look for it, THE VALUE on the API
+ //Separtion of concer: every function does only thing and do it right//
+let apiKey  = "fbef01f4et1b02o0d25c27210a43ef3f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}"&units=metric`;
+//console.log(apiUrl);//
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
+
+function handleSearchSubmit(event){
+  //update the H1 with the searched city//
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement= document.querySelector("#city");
-  cityElement.innerHTML= searchInput.value; 
+  
+  searchCity(searchInput.value); //sending the info to the searchCity function//
+
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", hadleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+
+searchCity("Jaen");
