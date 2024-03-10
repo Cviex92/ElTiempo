50e2ef07914205f3e279925e1043cc36677a9b61
Cviex92 <cviex92@gmail.com>
2024-03-10 18:57:51 +0000

    wind speed, weather description, and weather icon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ function refreshWeather(response){
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date= new Date (response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+
+  
+
 
   
   cityElement.innerHTML= response.data.city; 
@@ -15,6 +19,8 @@ function refreshWeather(response){
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   timeElement.innerHTML = formatDate(date);
+  icon.innerHTML = `<img src= "${response.data.condition.icon_url}" class ="weather-app-icon"/>`;
+  
 }
 
 //console.log(response.data); to check where the variables are-> respone.data.comdition.description//
